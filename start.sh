e545647c0dbe96de547a3b105e831cee7419806d
Niel Swart <niel@nielswart.com>
2021-12-02 14:45:11 +0200

    Add start script

---
+++ start.sh
@@ -0,0 +1,15 @@
+# backend
+cd ./backend
+docker-compose build
+docker-compose up
+
+# admin
+cd ./admin
+yarn install
+yarn start
+cd ..
+
+# one-page checkout
+cd ./one-page-checkout
+yarn install
+yarn start
